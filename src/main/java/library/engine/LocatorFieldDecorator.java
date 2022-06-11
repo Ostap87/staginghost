bf49ca9c27795d0fc38ebed1b3d13b1fba7f390e
BOTTOMLINE\oleksandr.ostapchuk <oleksandr.ostapchuk@bottomline.com>
2022-06-11 13:33:15 +0300

    Add exceptions, annotations with engine for PageObject

---
+++ src/main/java/library/engine/LocatorFieldDecorator.java
@@ -0,0 +1,21 @@
+package library.engine;
+
+import com.microsoft.playwright.Page;
+
+import java.lang.reflect.Field;
+
+public class LocatorFieldDecorator implements FieldDecorator {
+
+    Page page;
+    LocatorFactory locatorFactory;
+
+    public LocatorFieldDecorator(Page page) {
+        this.page = page;
+        this.locatorFactory = new LocatorFactory(page);
+    }
+
+    @Override
+    public Object decorate(Field field, Object pageObjectInstance) {
+        return locatorFactory.createLocator(field, pageObjectInstance);
+    }
+}
---
+++ src/main/java/library/engine/PageFactory.java
@@ -0,0 +1,146 @@
+package library.engine;
+
+import com.microsoft.playwright.Page;
+import library.annotations.FindChild;
+import library.annotations.Find;
+import library.annotations.PageObject;
+import library.exceptions.InvalidParentLocatorException;
+import library.exceptions.NoPageObjectAnnotation;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PageFactory {
+
+    public static <T> T create(Class<T> pageToCreate, Page page) {
+        return instantiatePage(pageToCreate, page, new LocatorFieldDecorator(page));
+    }
+
+    public static <T> T create(Class<T> pageToCreate, Page page, FieldDecorator decorator) {
+        return instantiatePage(pageToCreate, page, decorator);
+    }
+
+    public static void initElements(Object pageObject, Page page) {
+        if (pageObject.getClass().isAnnotationPresent(PageObject.class)) {
+            initElements(new LocatorFieldDecorator(page), pageObject);
+        } else {
+            throw new NoPageObjectAnnotation("Only pages marked with @PageObject can can be initialized by the PageFactory.");
+        }
+    }
+
+    private static void callAfterCreateHook(Object pageObjectInstance) {
+        if (AfterCreate.class.isAssignableFrom(pageObjectInstance.getClass())) {
+            ((AfterCreate) pageObjectInstance).afterCreate();
+        }
+    }
+
+    private static <T> T instantiatePage(Class<T> pageObjectClass, Page page, FieldDecorator decorator) {
+        try {
+            if (pageObjectClass.isAnnotationPresent(PageObject.class)) {
+                T pageObjectInstance;
+                try {
+                    Constructor<T> constructor = pageObjectClass.getConstructor(Page.class);
+                    pageObjectInstance = constructor.newInstance(page);
+                } catch (NoSuchMethodException e) {
+                    pageObjectInstance = pageObjectClass.getDeclaredConstructor().newInstance();
+                }
+                initElements(decorator, pageObjectInstance);
+                callAfterCreateHook(pageObjectInstance);
+                return pageObjectInstance;
+            }
+            throw new NoPageObjectAnnotation("Only pages marked with @PageObject can can be created by the PageFactory.");
+
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void initElements(FieldDecorator decorator, Object pageObjectInstance) {
+        List<Class<?>> classes = new ArrayList<>();
+        Class<?> pageObjectClass = pageObjectInstance.getClass();
+
+        while (pageObjectClass != Object.class) {
+            classes.add(pageObjectClass);
+            pageObjectClass = pageObjectClass.getSuperclass();
+        }
+
+        // Initialize in reverse order so base classes get initialized first
+        // so its fields are available to child classes
+        Collections.reverse(classes);
+        proxyFields(decorator, pageObjectInstance, classes);
+    }
+
+    private static void proxyFields(FieldDecorator decorator, Object pageObjectInstance, List<Class<?>> classes) {
+
+        List<String> fieldNamesAlreadyProxied = new ArrayList<>();
+        List<Field> fieldsWithDependencies = new ArrayList<>();
+
+        for (Class<?> pageObjectClass : classes) {
+            Field[] fields = pageObjectClass.getDeclaredFields();
+
+            for (Field field : fields) {
+                if (isALocator(field)) {
+                    if (hasDependencies(field)) {
+                        fieldsWithDependencies.add(field);
+                        continue;
+                    }
+                    setField(decorator, field, pageObjectInstance);
+                    fieldNamesAlreadyProxied.add(field.getName());
+                }
+            }
+
+            int sizeBefore;
+            while (!fieldsWithDependencies.isEmpty()) {
+                sizeBefore = fieldsWithDependencies.size();
+                List<Field> proxiedScopedFields = new ArrayList<>();
+
+                for (Field field : fieldsWithDependencies) {
+                    List<String> dependencyNames = List.of(field.getAnnotation(FindChild.class).value());
+
+                    if (dependencyNames.containsAll(fieldNamesAlreadyProxied)) {
+                        setField(decorator, field, pageObjectInstance);
+                        fieldNamesAlreadyProxied.add(field.getName());
+                        proxiedScopedFields.add(field);
+                    }
+                }
+
+                for (Field proxied : proxiedScopedFields) {
+                    fieldsWithDependencies.remove(proxied);
+                }
+
+                if (sizeBefore == fieldsWithDependencies.size()) {
+                    String fieldNames = fieldsWithDependencies.stream().map(Field::getName).collect(Collectors.joining(", "));
+                    String message = String.format(
+                            "\nUnable to find dependencies for the following Fields:\nPage Object: %s\nFields: %s",
+                            pageObjectClass.getName(), fieldNames);
+
+                    throw new InvalidParentLocatorException(message);
+                }
+            }
+        }
+    }
+
+    private static boolean hasDependencies(Field field) {
+        return field.isAnnotationPresent(FindChild.class);
+    }
+
+    private static boolean isALocator(Field field) {
+        return field.isAnnotationPresent(Find.class);
+    }
+
+    private static void setField(FieldDecorator decorator, Field field, Object pageObjectInstance) {
+        Object value = decorator.decorate(field, pageObjectInstance);
+        if (value != null) {
+            try {
+                field.setAccessible(true);
+                field.set(pageObjectInstance, value);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
